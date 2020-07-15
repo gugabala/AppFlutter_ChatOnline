5f1663301779aab3703dcb0508f575201f9b35c6
gugabala <gugabala@gmail.com>
2020-07-15 16:26:38 -0300

    conexão firebase

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(MyApp());
+  Firestore.instance
+      .collection("mensagens")
+      .document("msg1")
+      .setData({"texto": "Olá", "from": "Gustavo", "read": false});
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
