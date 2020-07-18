0fd6546acfb92a6aa0020c4ce7eee184387ea644
gugabala <gugabala@gmail.com>
2020-07-17 21:43:00 -0300

    conexão firebase e testes

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,44 @@ import 'package:flutter/material.dart';
 
 void main() async {
   runApp(MyApp());
+/*
   Firestore.instance
       .collection("mensagens")
+      .document("pYddC6Cjw2dl9uwnelTb")
+      .setData({"texto": "Olá", "from": "Ted", "read": false});
+
+  QuerySnapshot snapshot =
+      await Firestore.instance.collection("mensagens").getDocuments();
+  snapshot.documents.forEach((d) {
+    print(d.data);
+  });
+
+
+  DocumentSnapshot snapshot =
+      await Firestore.instance.collection("mensagens").document("msg1").get();
+  print(snapshot.data);
+
+    QuerySnapshot snapshot =
+      await Firestore.instance.collection("mensagens").getDocuments();
+  snapshot.documents.forEach((d) {
+    d.reference.updateData({"lido": false});
+  });
+
+   Firestore.instance.collection("mensagens").snapshots().listen((dado) {
+    dado.documents.forEach((d) {
+      print(d.data);
+    });
+  });
+
+
+    Firestore.instance
+      .collection("mensagens")
       .document("msg1")
-      .setData({"texto": "Olá", "from": "Gustavo", "read": false});
+      .snapshots()
+      .listen((dado) {
+    print(dado.data);
+  });
+  */
 }
 
 class MyApp extends StatelessWidget {
