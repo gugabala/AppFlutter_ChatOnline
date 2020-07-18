52eb94a4ceac5328b7a4e4d9a02347ffa2b9e236
gugabala <gugabala@gmail.com>
2020-07-17 22:21:56 -0300

    Enviando o Texto para o Firebase

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:chat/text_composer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -7,6 +8,10 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  void _sendMessage(String text) {
+    Firestore.instance.collection("messages").add({'text': text});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,11 +19,7 @@ class _ChatScreenState extends State<ChatScreen> {
         title: Text("olá"),
         elevation: 0,
       ),
-      body: TextComposer(
-          (texto){
-            print(texto);
-          }
-      ),
+      body: TextComposer(_sendMessage),
     );
   }
 }
--- lib/text_composer.dart
+++ lib/text_composer.dart
@@ -10,7 +10,16 @@ class TextComposer extends StatefulWidget {
 }
 
 class _TextComposerState extends State<TextComposer> {
+  final TextEditingController _controller = TextEditingController();
+
   bool _isComposing = false;
+
+  void _reset() {
+    _controller.clear();
+    setState(() {
+      _isComposing = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +33,7 @@ class _TextComposerState extends State<TextComposer> {
           ),
           Expanded(
             child: TextField(
+              controller: _controller,
               decoration:
                   InputDecoration.collapsed(hintText: "Enviar uma Mesagem"),
               onChanged: (text) {
@@ -33,12 +43,18 @@ class _TextComposerState extends State<TextComposer> {
               },
               onSubmitted: (text) {
                 widget.sendMessage(text);
+                _reset();
               },
             ),
           ),
           IconButton(
             icon: Icon(Icons.send),
-            onPressed: _isComposing ? () {} : null,
+            onPressed: _isComposing
+                ? () {
+                    widget.sendMessage(_controller.text);
+                    _reset();
+                  }
+                : null,
           )
         ],
       ),
